63dbb35ff4ef9620ce136019b7c7175fe18b0429
KacpeSK <142713164+KacpeSK@users.noreply.github.com>
2023-09-13 20:16:10 +0200

    Just routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,6 +28,6 @@ Route::get('/post/{id}', function ($id) {
     return response('Post ' . $id);
 })->where('id', '[0-9]+');
 
-Route::get('/search' function (Request $request) {
-
-})
+Route::get('/search', function (Request $request) {
+    dd($request);
+});
