f8e9347f40d4923c37a554dd1fab04fc0836ea6a
KacpeSK <142713164+KacpeSK@users.noreply.github.com>
2023-09-13 20:08:49 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return response('<h1>Hello world</h1>',200)
+    ->header('Content-Type', 'text/plain');
+});
+
+Route::get('/post/{id}', function ($id) {
+    ddd($id);
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search' function (Request $request) {
+
+})
